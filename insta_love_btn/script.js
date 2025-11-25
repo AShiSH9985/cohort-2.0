4d8f9747bbb87d575852921461ab33f195461187
Ashish <ashish8979778599@gmail.com>
2025-11-25 15:44:59 +0530

    insta Like

---
+++ insta_love_btn/script.js
@@ -0,0 +1,13 @@
+let img=document.querySelector("img")
+let icon=document.querySelector("i")
+
+img.addEventListener("dblclick",function(){
+   icon.style.opacity=0.8
+   setTimeout(() => {
+    icon.style.opacity=0
+   }, 1000);
+   setTimeout(() => {
+    icon.style.transform="translate(-50%,-50%) scale(0)"
+   }, 1200);
+   
+})
